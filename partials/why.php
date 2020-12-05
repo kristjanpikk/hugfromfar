1ba35189770d9ab41067ee8ed715bd63048328e0
Kristjan Pikk <kristjan.pikk@outlook.com>
2020-12-05 20:07:38 +0200

    Title fix

--- partials/why.php
+++ partials/why.php
@@ -10,9 +10,9 @@
 <!-- This example requires Tailwind CSS v2.0+ -->
 <div class="py-12 lg:py-32 bg-white">
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-    <p class="lg:text-center mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
+    <h2 class="lg:text-center mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">
       Why we do it?
-    </p>
+    </h2>
 
     <div class="mt-10 lg:mt-24">
       <dl class="space-y-10 md:space-y-0 md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
